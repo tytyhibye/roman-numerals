52101813b3422bd867dd95842c14828ef011e518
Tyler Bates <Fondlytyler@gmail.com>
2020-04-02 14:24:49 -0700

    utilize multiple arrays by looping through and comparing numerical values within said loops to return desired numerals.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,45 @@
 //business logic //
-  var translate = function(int) {
-    var numeralIndex = ["","I","II","III","IV","V","VI","VII","VII","IX"];
-    var numeral = "";
-    var digits = int.toString().split('');
-    for (var i=0; i <digits.length; i++) {
-      numeral = numeralIndex[(digits[i])];
-      console.log(numeral);
-      return numeral
-    }    
+  var array = [[1000, "M"],[900, "CM"],[500, "D"],[400, "CD"],[100, "C"],[90, "XC"],[50, "L"],[40, "XL"],[10, "X"],[9, "IX"],[5, "V"],[4, "IV"],[1, "I"]];
+
+  function translate(int) {
+    if(int === 0){
+      return ""
+    }
+    for(i=0; i< array.length; i++) {
+      if(int >=array[i][0]){
+        return array[i][1] + translate(int-array[i][0]);
+      }
+    }
   }
-
-
+  
+    
+    
+  
+  
 
 //User interface logic //
 $(document).ready(function(){
  $("#romanNumerals").submit(function(event){
    event.preventDefault();
   
-    var int = parseInt($("input#int").val());
+    var int = $("input#int").val();
     var result = translate(int);
-    // console.log(numeral);
     if (result) {
     $(".return").text(result);
+    console.log(result);
     }
   });
- });
+});
+
+  // var translate = function(int) {
+  // var onesIndex = ["","I","II","III","IV","V","VI","VII","VIII","IX"];
+    // var tensIndex = ["","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"];
+    
+    // if(int.length <2){
+    //   int = onesIndex[parseInt(int)]
+    //   return int
+    // }else if(int.length <3){
+    //   newArray = int.split('').reverse();
+    //   return (tensIndex[parseInt(newArray[1])]) + (onesIndex[parseInt(newArray[2])])
+      
+    // };
