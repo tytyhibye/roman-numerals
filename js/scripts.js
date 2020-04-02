8e72cb310c3386338f5940a6b98aa3e96618af91
Tyler Bates <Fondlytyler@gmail.com>
2020-04-02 11:03:08 -0700

    add numbers 4-9 to conversion algorithm.

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,14 @@
 //business logic //
   var translate = function(int) {
-    var numeralIndex = ["","I","II","III"];
+    var numeralIndex = ["","I","II","III","IV","V","VI","VII","VII","IX"];
     var numeral = "";
     var digits = int.toString().split('');
     for (var i=0; i <digits.length; i++) {
       numeral = numeralIndex[(digits[i])];
       console.log(numeral);
       return numeral
-    }
-    
+    }    
   }
-
-
-// var numeralCodes = [["","I","II","III","IV","V","VI","VII","VIII","IX"],         // Ones
-//                     ["","X","XX","XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],   // Tens
-//                     ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"]];        // Hundreds
-
-// function convert(int) {
-//   var numeral = "";
-//   var digits = Int.toString().split('').reverse();
-//   for (var i=0; i < digits.length; i++){
-//     numeral = numeralCodes[i][parseInt(digits[i])] + numeral;
-//   }
-//   return numeral;  
-// }
-
 
 
 
